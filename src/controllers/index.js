bda34fe04b08e18d511b38e4140dc52e5e2b6449
Jahongir <zokirovjahongir456@gmail.com>
2025-01-14 17:26:23 +0500

    3-month 4-lesson
    Home work of 34-lesson

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,2 +1,5 @@
-export * from './authorController.js';
-export * from './categoryController.js';
+export * from "./auth.controller.js"
+export * from "./blog.controller.js"
+export * from "./user.controller.js"
+export * from './category.controller.js'
+export * from './article.controller.js'
